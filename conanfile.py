fc034833560539641dd3f17082b58c274a13be74
Fred Malouf <fred.malouf@nio.io>
2020-08-07 16:44:01 -0700

    Update conan file

---
+++ conanfile.py
@@ -0,0 +1,22 @@
+from conans import ConanFile, tools
+
+# Must first use Maven to build binaries before using this file - see README
+
+import os
+
+class CapicxxcoretoolsConan(ConanFile):
+    name = "capicxx-core-tools"
+    version = "3.1.12.4"
+    settings = "os", "compiler", "build_type", "arch"
+    description = "Core tools built with maven"
+    url = "None"
+    license = "None"
+    author = "None"
+    topics = None
+
+    def package(self):
+        self.copy("*", "bin")
+
+    def package_info(self):
+        self.env_info.PATH.append(os.path.join(self.package_folder, "bin"))
+
